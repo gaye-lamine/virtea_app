6031b756259d955c7d9174a033c97505c02f2012
Lamine Gaye <lamine.gaye@yoniafrica.com>
2026-01-20 16:33:26 +0000

    update

--- config/database.ts
+++ config/database.ts
@@ -2,17 +2,12 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: 'pg', 
   connections: {
-    mysql: {
-      client: 'mysql2',
-      connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
-      },
+    pg: {
+      client: 'pg', 
+      connection: env.get('DATABASE_URL'), 
+      debug: env.get('NODE_ENV') !== 'production',
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
